0a5931d407b104f545f61a9f4d19276f06c44c82
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-29 10:44:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+const newNumbers = numbers.map((x) => x * x);
+console.log(newNumbers);
+//////Filter - Create a new array by keeping the items that return true.
+const newNumbers2 = numbers.filter((num) => num < 10);
+console.log(newNumbers2);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+var newNumbers3 = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+console.log(newNumbers3);
+
+////Find - find the first item that matches from an array.
+const newNumber4 = numbers.find((num) => num > 10);
+console.log(newNumber4);
+
+////FindIndex - find the index of the first item that matches.
+const newNumber5 = numbers.findIndex((num) => num > 10);
+console.log(newNumber5);
